7026189dac92748c36d418d48ef2bebb01be4ab4
ka0824 <rkgud0824@gmail.com>
2024-04-04 13:19:30 +0900

    feat: text ellipsis

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -11,18 +11,20 @@ const ItemList = () => {
   };
 
   return (
-    <>
+    <div>
       {list.map((item, index) => (
-        <div key={`item-${index}`}>
+        <div key={`item-${index}`} className="flex gap-4 mb-1">
           <span className={`bg-[${colors[index % colors.length]}]`}>
             {"\u00A0"}
           </span>
-          <span>{item.name}</span>
+          <span className="inline-box w-24 text-ellipsis overflow-hidden">
+            {item.name}
+          </span>
           <span>x {item.count}</span>
         </div>
       ))}
       <AddItem addItem={addItem} />
-    </>
+    </div>
   );
 };
 
